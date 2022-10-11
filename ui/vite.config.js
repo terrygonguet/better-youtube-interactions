a63270d16259a2db00afd86d3830183ab3602ebc
terrygonguet <terry@gonguet.com>
2022-10-11 13:22:04 +0200

    build: :building_construction: Add custom svg files to build system

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from "vite"
 import { svelte } from "@sveltejs/vite-plugin-svelte"
 import Icons from "unplugin-icons/vite"
+import { FileSystemIconLoader } from "unplugin-icons/loaders"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [svelte({ emitCss: false }), Icons({ compiler: "svelte" })],
+	plugins: [
+		svelte({ emitCss: false }),
+		Icons({
+			compiler: "svelte",
+			customCollections: {
+				custom: FileSystemIconLoader("./assets/svg"),
+			},
+		}),
+	],
 	build: {
 		lib: {
 			entry: "src/main.js",
